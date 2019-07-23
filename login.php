e040f68049f339299a244276ac49bf210d2a1cc3
Arturo92 <EamesAguilera@gmail.com>
2019-07-23 09:22:17 -0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+$link = mysqli_connect("localhost", "root", "Csharp92", "databas");
+$username = mysqli_real_escape_string($link, $_POST['username']); 
+$password = mysqli_real_escape_string($link, $_POST['password']); 
+
+$check = mysqli_query($link, "SELECT * FROM `users` WHERE `username`='$username'" ) or die (mysql_error());
+$numrows = mysqli_num_rows($check);
+if ($numrows == 0)
+	die("Username doesn't exist");
+else {
+	while ($row = mysqli_fetch_assoc($check)) {
+		if($row['password'] == $password){
+			echo $username;
+		}
+		else {
+			die("");
+		}
+	}
+}
+?>
---
+++ login_script.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class login_script : MonoBehaviour {
+	
+	public Text emailText;
+	public Text passwordText;
+	public Text responseText;
+	public Text errorText;
+	public Button loginButton;
+	public Button logoutButton;
+	
+	public List<String> errors;
+	private string url_login = "http://localhost/unityloginexample/login.php";
+	
+	public void DoLogin() {
+		StartCoroutine(Login());
+	}
+	
+	public void DoLogout(){
+		StartCoroutine(Logout());
+	}
+    IEnumerator Login() {
+		WWWForm form = new WWWForm();
+		form.AddField("username", emailText.text);
+		form.AddField("password", passwordText.text);
+		WWW www = new WWW(url_login, form);
+        yield return www;
+		
+			    errors.Add("Cannot Leave Username Empty");
+				errors.Add("Cannot Leave Password Empty");
+				errors.Add("Cannot Leave Username/Password Empty");
+				
+				if(emailText.text != "" && passwordText.text != ""){
+					errorText.text = "";
+					responseText.text = www.text;
+					loginButton.gameObject.SetActive(false);
+				}
+				
+			    if(emailText.text == ""){
+					errors.Add("Cannot Leave Username Empty");
+					errorText.text = errors[0];
+                    Debug.Log("Form error");
+			    } 
+				
+				if(passwordText.text == ""){
+					errorText.text = errors[1];
+					Debug.Log("Form error");
+				}
+				
+				if(emailText.text == "" && passwordText.text == ""){
+					errorText.text = errors[2];
+					Debug.Log("Form error");
+				}
+				
+				if(www.text == "Username doesn't exist"){
+					loginButton.gameObject.SetActive(true);
+				}
+	}
+	
+	IEnumerator Logout(){
+		yield return responseText.text = "";
+		loginButton.gameObject.SetActive(true);
+	}
+    void Start() {
+	}
+	
+	void Update() {
+		
+	}
+}
